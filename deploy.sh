370ef665869eec88b61e55596e1df12f3bf2ad59
Bill Huang <bill.huang@trufo.ai>
2025-09-03 03:17:34 -0400

    Fixing GCP deploy script. Removing an old css reference.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@
 # --- Configuration ---
 BUCKET_NAME="ibc2025-c2pa-01"
 SUBDIRECTORY="c2pa-dash"
+
+CACHE_CONTROL_HEADER="Cache-Control:no-cache, max-age=0"
 # ---------------------
 
 echo "🚀 Starting sync to gs://${BUCKET_NAME}/${SUBDIRECTORY}/"
@@ -13,6 +15,6 @@ echo "🚀 Starting sync to gs://${BUCKET_NAME}/${SUBDIRECTORY}/"
 # The -d flag deletes files in the destination that don't exist in the source.
 # The -r flag makes the sync recursive.
 # The -x flag excludes specified file patterns from being uploaded.
-gcloud storage rsync . gs://${BUCKET_NAME}/${SUBDIRECTORY} -r -d -x "\.git/.*|\.gitignore|README\.md|LICENSE|.*\.md$|deploy\.sh"
+gcloud storage rsync . gs://${BUCKET_NAME}/${SUBDIRECTORY} -r -x "\.git/.*|\.gitignore|README\.md|LICENSE|.*\.md$|deploy\.sh" --delete-unmatched-destination-objects
 
 echo "✅ Sync complete! Your website is updated."
